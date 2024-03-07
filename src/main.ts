b5c876b95ef5eba56e572314b60109e698e6b8e3
67au <oyy6745@gmail.com>
2024-03-07 17:02:32 +0800

    pref: reduce bundle size

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import Varlet from '@varlet/ui'
-import '@varlet/ui/es/style'
+// import Varlet from '@varlet/ui'
+// import '@varlet/ui/es/style'
 import '@varlet/touch-emulator'
 
 import App from './App.vue'
@@ -8,4 +8,5 @@ import { i18n } from '@/i18n'
 
 const app = createApp(App)
 
-app.use(Varlet).use(i18n).mount('#app')
+// app.use(Varlet)
+app.use(i18n).mount('#app')
